7688528050cd1a29ad612ba34d7cfb71cd947a19
nacho.rod91 <juanignacio.rodriguezsilva@cognizant.com>
2020-06-20 13:17:43 +0200

    input component mock test for inputbox value

--- src/Input.js
+++ src/Input.js
@@ -2,9 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function Input({ secretWord }) {
+  // in order to mock the Hook, we need to do React.useState instead of destructuring
+  // the hook in the import and just use useState
+  const [currentGuess, setCurrentGuess] = React.useState("");
   return (
     <div data-test="component-input">
-      
+      <form className="form-inline">
+        <input 
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="Enter guess"
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2">
+           Submit    
+        </button>
+      </form>
     </div>
   )
 }
